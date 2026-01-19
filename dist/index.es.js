abed3aab7ba92620e53336ba80e0061247f64ac2
Elias Lazcano Castro Neto <elias304162@gmail.com>
2026-01-19 18:10:21 -0400

    Recompilado

--- dist/index.es.js
+++ dist/index.es.js
@@ -1,6 +1,6 @@
 import { defineStore as v } from "pinia";
-import { getCurrentInstance as c, onMounted as _, watch as y } from "vue";
-const m = v("vuetifyComponentsStore", {
+import { getCurrentInstance as m, onMounted as _, watch as y } from "vue";
+const i = v("vuetifyComponentsStore", {
   state: () => ({
     loaderOverlayShow: !1,
     loaderDialogShow: !1,
@@ -39,9 +39,9 @@ const m = v("vuetifyComponentsStore", {
       this.prompts.push(t);
     },
     removerPrompt(s, t = !1, o = !1) {
-      var e, a, i;
+      var e, a, l;
       const r = this.prompts.findIndex((n) => n.id === s);
-      this.prompts[r].show = !1, t && typeof ((e = this.prompts[r].options) == null ? void 0 : e.onCancel) == "function" && this.prompts[r].options.onCancel(), o && typeof ((a = this.prompts[r].options) == null ? void 0 : a.onDismiss) == "function" && this.prompts[r].options.onDismiss(), typeof ((i = this.prompts[r].options) == null ? void 0 : i.onFinally) == "function" && this.prompts[r].options.onFinally(), setTimeout(() => this.prompts.splice(r, 1), 800);
+      this.prompts[r].show = !1, t && typeof ((e = this.prompts[r].options) == null ? void 0 : e.onCancel) == "function" && this.prompts[r].options.onCancel(), o && typeof ((a = this.prompts[r].options) == null ? void 0 : a.onDismiss) == "function" && this.prompts[r].options.onDismiss(), typeof ((l = this.prompts[r].options) == null ? void 0 : l.onFinally) == "function" && this.prompts[r].options.onFinally(), setTimeout(() => this.prompts.splice(r, 1), 800);
     },
     resetarPrompts() {
       this.prompts.forEach((s, t) => {
@@ -49,12 +49,26 @@ const m = v("vuetifyComponentsStore", {
       }), setTimeout(() => this.prompts = [], 300);
     }
   }
-});
-function p(s, t, o, r, e, a, i, n) {
-  var l = typeof s == "function" ? s.options : s;
-  return t && (l.render = t, l.staticRenderFns = o, l._compiled = !0), {
+}), V = (s, t = "") => {
+  const o = i();
+  o.loaderText = t, o.loaderOverlayShow = s;
+}, B = () => i().loaderOverlayShow, j = (s, t = "") => {
+  const o = i();
+  o.loaderText = t, o.loaderDialogShow = s;
+}, R = (s) => {
+  i().addSnack(s);
+}, M = (s) => {
+  i().addAlert(s);
+}, N = (s) => {
+  i().showDialog(s);
+}, U = (s) => {
+  i().adicionarPrompt(s);
+};
+function c(s, t, o, r, e, a, l, n) {
+  var p = typeof s == "function" ? s.options : s;
+  return t && (p.render = t, p.staticRenderFns = o, p._compiled = !0), {
     exports: s,
-    options: l
+    options: p
   };
 }
 const h = {
@@ -69,15 +83,15 @@ const h = {
     return { __sfc: !0 };
   }
 };
-var g = function() {
+var C = function() {
   var t = this, o = t._self._c;
   return t._self._setupProxy, o("v-overlay", t._b({ attrs: { value: t.value, "z-index": t.zIndex, opacity: t.opacity } }, "v-overlay", t.$attrs, !1), [o("div", { staticClass: "text-center" }, [o("v-progress-circular", { attrs: { indeterminate: "", size: "48" } }), o("div", { staticClass: "pt-3" }, [t.$slots.default ? t._t("default") : t.texto ? [t._v(t._s(t.texto))] : t._e()], 2)], 1)]);
-}, C = [], x = /* @__PURE__ */ p(
+}, S = [], g = /* @__PURE__ */ c(
   h,
-  g,
-  C
+  C,
+  S
 );
-const d = x.exports, b = {
+const d = g.exports, x = {
   __name: "EGlobalLoaderDialog",
   props: {
     value: { type: Boolean, default: !1 },
@@ -92,14 +106,14 @@ const d = x.exports, b = {
     return { __sfc: !0 };
   }
 };
-var S = function() {
+var b = function() {
   var t = this, o = t._self._c;
   return t._self._setupProxy, o("v-dialog", t._b({ attrs: { value: t.value, "hide-overlay": t.hideOverlay, persistent: t.persistent, width: t.width } }, "v-dialog", t.$attrs, !1), [o("v-card", { attrs: { color: t.color, dark: "" } }, [o("v-card-text", { staticClass: "text-center pt-1" }, [t._t("default", function() {
     return [t._v(t._s(t.text ? t.text : "Por favor aguarde"))];
   }), o("v-progress-linear", { staticClass: "mb-0", attrs: { indeterminate: "", color: t.progressColor } })], 2)], 1)], 1);
-}, w = [], k = /* @__PURE__ */ p(
+}, w = [], k = /* @__PURE__ */ c(
+  x,
   b,
-  S,
   w
 );
 const u = k.exports, $ = {
@@ -114,32 +128,32 @@ const u = k.exports, $ = {
   },
   emits: ["close"],
   setup(s) {
-    const { proxy: t } = c(), o = t.$vuetify;
+    const { proxy: t } = m(), o = t.$vuetify;
     return { __sfc: !0, proxy: t, vuetify: o };
   }
 };
-var P = function() {
+var O = function() {
   var t = this, o = t._self._c, r = t._self._setupProxy;
   return o("v-toolbar", t._b({ staticClass: "flex-grow-0", attrs: { color: r.vuetify.framework.theme.dark && t.color === "primary" ? void 0 : t.color, dark: "", flat: "", dense: "" } }, "v-toolbar", t.$attrs, !1), [t.icon ? o("v-icon", { staticClass: "mr-2", attrs: { size: t.iconSize } }, [t._v(t._s(t.icon))]) : t._e(), o("v-toolbar-title", { staticStyle: { "line-height": "1.1" }, style: { fontSize: t.titleSize } }, [t._v(t._s(t.title))]), o("v-spacer"), t._t("default"), t.btnClose ? o("v-btn", { attrs: { icon: "" }, on: { click: function(e) {
     return t.$emit("close");
   } } }, [o("v-icon", [t._v("mdi-close")])], 1) : t._e()], 2);
-}, O = [], D = /* @__PURE__ */ p(
+}, P = [], D = /* @__PURE__ */ c(
   $,
-  P,
-  O
+  O,
+  P
 );
 const f = D.exports, T = {
   __name: "EGlobalUI",
   setup(s) {
-    const t = m(), { proxy: o } = c(), r = o.$vuetify;
+    const t = i(), { proxy: o } = m(), r = o.$vuetify;
     return _(() => {
       o.$route && y(
         () => o.$route.fullPath,
         () => t.resetarPrompts()
       );
-    }), { __sfc: !0, vuetifyComponentsStore: t, proxy: o, vuetify: r, promptOnOk: (a, i, n = () => {
-    }, l = "") => {
-      a.validate() && (n(l), t.removerPrompt(i));
+    }), { __sfc: !0, vuetifyComponentsStore: t, proxy: o, vuetify: r, promptOnOk: (a, l, n = () => {
+    }, p = "") => {
+      a.validate() && (n(p), t.removerPrompt(l));
     }, EGlobalLoaderOverlay: d, EGlobalLoaderDialog: u, VCardToolbar: f };
   }
 };
@@ -154,14 +168,14 @@ var z = function() {
       return r.vuetifyComponentsStore.removeSnack(e.id);
     } } }, [e.icon ? o("v-icon", { staticClass: "mr-1", attrs: { dense: "", color: "white" } }, [t._v(t._s(e.icon))]) : e.color === "success" ? o("v-icon", { staticClass: "mr-1", attrs: { dense: "", color: "white" } }, [t._v("mdi-check-circle")]) : t._e(), t._v(" " + t._s(e.text) + " ")], 1);
   }), t._l(r.vuetifyComponentsStore.prompts, function(e, a) {
-    var i;
+    var l;
     return o("v-dialog", { key: e.id, attrs: { width: e.options.width || "24em", persistent: e.options.persistent, "hide-overlay": e.options.hideOverlay }, on: { "click:outside": function(n) {
       e.options.persistent || r.vuetifyComponentsStore.removerPrompt(e.id, !1, !0);
     } }, model: { value: e.show, callback: function(n) {
       t.$set(e, "show", n);
     }, expression: "i.show" } }, [o("v-card", [o("v-form", { ref: "prompters", refInFor: !0, on: { submit: function(n) {
       return n.preventDefault(), r.promptOnOk(t.$refs.prompters[a], e.id, e.options.onOk, e.promptValue);
-    } } }, [e.options.title ? o(r.VCardToolbar, { attrs: { title: e.options.title, color: e.options.titleColor || "primary", "hide-close-button": "" } }) : t._e(), e.options.message || e.options.prompt ? o("v-card-text", { staticClass: "text-justify pb-1" }, [e.options.message ? o("div", { class: e.options.messageClass || "my-2", domProps: { innerHTML: t._s(e.options.message) } }) : t._e(), e.options.prompt ? [((i = e.options.prompt) == null ? void 0 : i.type) === "textarea" ? o("v-textarea", { style: { marginTop: e.options.message ? "1.25rem" : "0" }, attrs: { label: e.options.prompt.label, rules: e.options.prompt.rules, outlined: e.options.prompt.outlined, filled: e.options.prompt.filled, placeholder: e.options.prompt.placeholder, persistentPlaceholder: e.options.prompt.persistentPlaceholder, dense: e.options.prompt.dense, autofocus: e.options.prompt.autofocus, rows: e.options.prompt.rows || 5 }, model: { value: e.promptValue, callback: function(n) {
+    } } }, [e.options.title ? o(r.VCardToolbar, { attrs: { title: e.options.title, color: e.options.titleColor || "primary", "hide-close-button": "" } }) : t._e(), e.options.message || e.options.prompt ? o("v-card-text", { staticClass: "text-justify pb-1" }, [e.options.message ? o("div", { class: e.options.messageClass || "my-2", domProps: { innerHTML: t._s(e.options.message) } }) : t._e(), e.options.prompt ? [((l = e.options.prompt) == null ? void 0 : l.type) === "textarea" ? o("v-textarea", { style: { marginTop: e.options.message ? "1.25rem" : "0" }, attrs: { label: e.options.prompt.label, rules: e.options.prompt.rules, outlined: e.options.prompt.outlined, filled: e.options.prompt.filled, placeholder: e.options.prompt.placeholder, persistentPlaceholder: e.options.prompt.persistentPlaceholder, dense: e.options.prompt.dense, autofocus: e.options.prompt.autofocus, rows: e.options.prompt.rows || 5 }, model: { value: e.promptValue, callback: function(n) {
       t.$set(e, "promptValue", n);
     }, expression: "i.promptValue" } }) : o("v-text-field", { directives: [{ name: "maska", rawName: "v-maska", value: e.options.prompt.mask || null, expression: "i.options.prompt.mask || null" }], style: { marginTop: e.options.message ? "1.25rem" : "0" }, attrs: { label: e.options.prompt.label, type: e.options.prompt.type || "text", rules: e.options.prompt.rules, outlined: e.options.prompt.outlined, filled: e.options.prompt.filled, placeholder: e.options.prompt.placeholder, persistentPlaceholder: e.options.prompt.persistentPlaceholder, dense: e.options.prompt.dense, autofocus: e.options.prompt.autofocus }, model: { value: e.promptValue, callback: function(n) {
       t.$set(e, "promptValue", n);
@@ -169,21 +183,32 @@ var z = function() {
       return r.vuetifyComponentsStore.removerPrompt(e.id, !0, !0);
     } } }, [t._v(t._s(e.options.cancelText || "CANCELAR"))]) : t._e(), o("v-btn", { class: e.options.confirmClass, attrs: { color: e.options.confirmColor || "primary", small: e.options.smallButtons, type: "submit", depressed: e.options.confirmDepressed ?? !0, outlined: e.options.confirmOutlined } }, [t._v(t._s(e.options.confirmText || "OK"))])], 1)], 1)], 1)], 1);
   })], 2);
-}, I = [], G = /* @__PURE__ */ p(
+}, L = [], I = /* @__PURE__ */ c(
   T,
   z,
-  I
+  L
 );
-const L = G.exports, V = {
-  install(s, t = {}) {
-    const o = t.pinia || s.prototype.$pinia;
-    if (!o) {
-      console.error("[@eliaslazcano/vuetify] Pinia é necessário para esta biblioteca.");
-      return;
-    }
-    s.prototype.$vuetifyComponentsStore = m(o), s.component("EGlobalUi", L), s.component("EGlobalLoaderOverlay", d), s.component("EGlobalLoaderDialog", u), s.component("VCardToolbar", f);
+const G = I.exports, E = (s, t = {}) => {
+  const o = t.pinia || s.prototype.$pinia;
+  if (!o) {
+    console.error("[@eliaslazcano/vuetify] Pinia é necessário para esta biblioteca.");
+    return;
   }
-};
+  s.prototype.$vuetifyComponentsStore = i(o), s.component("EGlobalUi", G), s.component("EGlobalLoaderOverlay", d), s.component("EGlobalLoaderDialog", u), s.component("VCardToolbar", f);
+}, H = { install: E };
 export {
-  V as default
+  u as EGlobalLoaderDialog,
+  d as EGlobalLoaderOverlay,
+  G as EGlobalUi,
+  f as VCardToolbar,
+  H as default,
+  M as emitirAlert,
+  N as emitirDialog,
+  U as emitirPrompt,
+  R as emitirSnack,
+  B as getLoaderOverlayStatus,
+  E as install,
+  j as setLoaderDialog,
+  V as setLoaderOverlay,
+  i as useVuetifyComponentsStore
 };
