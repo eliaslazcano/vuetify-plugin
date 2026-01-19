3bc5dde096417876bea518a88de657642bfe55f3
Elias Lazcano Castro Neto <elias304162@gmail.com>
2026-01-19 18:04:59 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,40 @@
-import { useVuetifyComponentsStore } from './store/vuetifyComponentsStore'
+import { useVuetifyComponentsStore, emitirAlert, emitirDialog, emitirPrompt, emitirSnack, setLoaderOverlay, setLoaderDialog, getLoaderOverlayStatus } from './store/vuetifyComponentsStore'
 import EGlobalUi from './components/EGlobalUI.vue'
 import EGlobalLoaderOverlay from './components/EGlobalLoaderOverlay.vue'
 import EGlobalLoaderDialog from './components/EGlobalLoaderDialog.vue'
 import VCardToolbar from './components/VCardToolbar.vue'
 
-export default {
-  install(Vue, options = {}) {
-
-    const pinia = options.pinia || (Vue.prototype.$pinia) // Tenta pegar do Vue se não for passado
-    if (!pinia) {
-      console.error('[@eliaslazcano/vuetify] Pinia é necessário para esta biblioteca.')
-      return
-    }
-
-    // inicializa store do pacote e disponibiliza global via prototype (Vue 2 way)
-    Vue.prototype.$vuetifyComponentsStore = useVuetifyComponentsStore(pinia)
-
-    // registra os componentes globais
-    Vue.component('EGlobalUi', EGlobalUi)
-    Vue.component('EGlobalLoaderOverlay', EGlobalLoaderOverlay)
-    Vue.component('EGlobalLoaderDialog', EGlobalLoaderDialog)
-    Vue.component('VCardToolbar', VCardToolbar)
+const install = (Vue, options = {}) => {
+  const pinia = options.pinia || (Vue.prototype.$pinia) // Tenta pegar do Vue se não for passado
+  if (!pinia) {
+    console.error('[@eliaslazcano/vuetify] Pinia é necessário para esta biblioteca.')
+    return
   }
+
+  // inicializa store do pacote e disponibiliza global via prototype (Vue 2 way)
+  Vue.prototype.$vuetifyComponentsStore = useVuetifyComponentsStore(pinia)
+
+  // registra os componentes globais (opcional, mantido para compatibilidade se o usuário usar Vue.use())
+  Vue.component('EGlobalUi', EGlobalUi)
+  Vue.component('EGlobalLoaderOverlay', EGlobalLoaderOverlay)
+  Vue.component('EGlobalLoaderDialog', EGlobalLoaderDialog)
+  Vue.component('VCardToolbar', VCardToolbar)
 }
+
+export {
+  install,
+  useVuetifyComponentsStore,
+  EGlobalUi,
+  EGlobalLoaderOverlay,
+  EGlobalLoaderDialog,
+  VCardToolbar,
+  emitirAlert,
+  emitirDialog,
+  emitirPrompt,
+  emitirSnack,
+  setLoaderOverlay,
+  setLoaderDialog,
+  getLoaderOverlayStatus
+}
+
+export default { install }
