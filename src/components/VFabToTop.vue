e2c1f4a89c9ac78b7d918f37152e138ef30a97d7
Elias Lazcano Castro Neto <elias304162@gmail.com>
2026-01-20 15:30:26 -0400

    Update VFabToTop.vue

--- src/components/VFabToTop.vue
+++ src/components/VFabToTop.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { getCurrentInstance, ref, onMounted } from 'vue'
+import { getCurrentInstance, ref } from 'vue'
 
 const props = defineProps({
   scrollOffset: {type: Number, default: 300},
@@ -10,16 +10,6 @@ const vuetify = proxy.$vuetify
 
 const exibirBotao = ref(false)
 const onScroll = () => exibirBotao.value = (window.pageYOffset || document.documentElement.offsetTop || 0) > props.scrollOffset
-
-console.log('vuetify: ', vuetify)
-console.log('vuetify?.application: ', vuetify?.application)
-console.log('vuetify?.application?.snackbar: ', vuetify?.application?.snackbar)
-
-onMounted(() => {
-  console.log('2vuetify: ', vuetify)
-  console.log('2vuetify?.application: ', vuetify?.application)
-  console.log('2vuetify?.application?.snackbar: ', vuetify?.application?.snackbar)
-})
 </script>
 
 <template>
@@ -29,7 +19,6 @@ onMounted(() => {
       v-scroll="onScroll"
       v-bind="$attrs"
       v-on="$listeners"
-      :style="{marginBottom: `${vuetify.application.snackbar}px`}"
       class="transition-swing"
       color="primary"
       style="z-index: 6"
